0daff137279544a28221faa52ce52df1f027f482
Snnacky <3258595465@qq.com>
2026-05-16 15:15:13 +0800

    更新资源

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,10 +11,11 @@ using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    public RawImage image;
-    private void Start()
+    public Image image;
+    async void Start()
     {
-        Initialize().Forget();
+        await Initialize();
+        Tast().Forget();
     }
 
     async UniTask Initialize()
@@ -78,83 +79,15 @@ public class Main : MonoBehaviour
 
     async UniTask Tast()
     {
-        var handle1 = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Capsule.prefab");
+        var handle1 = Addressables.LoadAssetAsync<GameObject>("Circle");
         await handle1.Task;
         GameObject prefab = handle1.Result;
         GameObject cube = Instantiate(prefab);
-        
+
+        var handle2 = Addressables.LoadAssetAsync<Sprite>("P1");
+        await handle2.Task;
+        Sprite sp = handle2.Result;
+        image.GetComponent<SpriteRenderer>().sprite = sp;
+
     }
 }
-
-/*
-public class AddressableUpdateManager : MonoBehaviour
-{
-    async void Start()
-    {
-        // 1. 初始化 Addressables
-        await Addressables.InitializeAsync().Task;
-
-        // 2. 检查目录更新
-        // 检查远程服务器上的 catalog 是否比本地新
-        var checkHandle = Addressables.CheckForCatalogUpdates(false);
-        await checkHandle.Task;
-
-        if (checkHandle.Status == AsyncOperationStatus.Succeeded && checkHandle.Result.Count > 0)
-        {
-            Debug.Log("检测到资源更新，正在更新目录...");
-            // 3. 更新目录
-            var updateHandle = Addressables.UpdateCatalogs(checkHandle.Result, false);
-            await updateHandle.Task;
-            
-            // 更新完成后释放 handle
-            Addressables.Release(updateHandle);
-        }
-        Addressables.Release(checkHandle);
-
-        // 4. 下载所有受更新影响或缺失的资源
-        await DownloadAllAssets();
-    }
-
-    async System.Threading.Tasks.Task DownloadAllAssets()
-    {
-        // 获取所有资源定位符中的所有 Keys
-        List<object> allKeys = new List<object>();
-        foreach (var locator in Addressables.ResourceLocators)
-        {
-            allKeys.AddRange(locator.Keys);
-        }
-
-        // 5. 获取下载大小
-        var sizeHandle = Addressables.GetDownloadSizeAsync(allKeys);
-        await sizeHandle.Task;
-        long totalSize = sizeHandle.Result;
-
-        if (totalSize > 0)
-        {
-            Debug.Log($"需要下载的资源大小: {totalSize / 1024f / 1024f:F2} MB");
-
-            // 6. 开始下载
-            var downloadHandle = Addressables.DownloadDependenciesAsync(allKeys, Addressables.MergeMode.Union);
-            
-            while (!downloadHandle.IsDone)
-            {
-                float progress = downloadHandle.PercentComplete;
-                Debug.Log($"下载进度: {progress * 100}%");
-                yield return null; // 如果在协程中，这里可以使用 yield
-            }
-
-            if (downloadHandle.Status == AsyncOperationStatus.Succeeded)
-            {
-                Debug.Log("所有资源下载更新完成！");
-            }
-            Addressables.Release(downloadHandle);
-        }
-        else
-        {
-            Debug.Log("没有需要更新的资源内容。");
-        }
-        
-        Addressables.Release(sizeHandle);
-    }
-}
- */
